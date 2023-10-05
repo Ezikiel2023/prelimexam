f0b75a564185daea3b0146e04d7e54768435992f
Ezikiel2023 <144298766+Ezikiel2023@users.noreply.github.com>
2023-10-05 15:57:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,80 +1,80 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function CalButton({ label, onClick }) {
+function CalcButton({ label, onClick }) {
   return (
-    <button className="CalButton" onClick={() => onClick(label)}>
+    <button onClick={() => onClick(label)}>
       {label}
     </button>
   );
 }
 
-function CalcDisplay({ expression }) {
+function CalcDisplay({ display }) {
   return (
     <div className='CalcDisplay'>
-      {expression}
+      {display}
     </div>
   );
 }
 
 function App() {
-  const [expression, setExpression] = useState('');
+  const [displayValue, setDisplayValue] = useState('');
+  const [calculation, setCalculation] = useState('');
 
-  const handleButtonClick = (value) => {
-    switch (value) {
-      case '=':
-        calculate();
-        break;
-      case 'C':
-        clearDisplay();
-        break;
-      default:
-        appendValue(value);
-        break;
+  const handleButtonClick = (buttonLabel) => {
+    if (buttonLabel === '=') {
+      try {
+        
+        const result = eval(calculation);
+        setDisplayValue(result.toString());
+        setCalculation(result.toString());
+      } catch (error) {
+        
+        setDisplayValue('Error');
+        setCalculation('');
+      }
+    } else if (buttonLabel === 'C') {
+      
+      setDisplayValue('');
+      setCalculation('');
+    } else {
+    
+      setCalculation((prevCalculation) => prevCalculation + buttonLabel);
+      setDisplayValue((prevDisplayValue) => prevDisplayValue + buttonLabel);
     }
   };
-
-  const appendValue = (value) => {
-    setExpression((prevExpression) => prevExpression + value);
+  const handleSurnameButtonClick = () => {
+    // Add your full name to the display
+    setDisplayValue('Kenneth Clark G. Auguis');
   };
 
-  const clearDisplay = () => {
-    setExpression('');
-  };
-
-  const calculate = () => {
-    try {
-      let result = eval(expression);
-      setExpression(result.toString());
-    } catch (error) {
-      setExpression('Error');
-    }
-  };
 
   return (
     <div className='CalcContainer'>
-      <h1 className="Header">Calculator of Ezikiel Dizon Manese IT3A</h1>
-      <CalcDisplay expression={expression} />
+ <h1 className='CalculatorTitle'>Calculator of Kenneth Clark G. Auguis - IT3A</h1>
+      <CalcDisplay display={displayValue} />
       <div className='ButtonContainer'>
-        <CalButton label={7} onClick={handleButtonClick} />
-        <CalButton label={8} onClick={handleButtonClick} />
-        <CalButton label={9} onClick={handleButtonClick} />
-        <CalButton label={"/"} onClick={handleButtonClick} />
-        <CalButton label={4} onClick={handleButtonClick} />
-        <CalButton label={5} onClick={handleButtonClick} />
-        <CalButton label={6} onClick={handleButtonClick} />
-        <CalButton label={"*"} onClick={handleButtonClick} />
-        <CalButton label={1} onClick={handleButtonClick} />
-        <CalButton label={2} onClick={handleButtonClick} />
-        <CalButton label={3} onClick={handleButtonClick} />
-        <CalButton label={"-"} onClick={handleButtonClick} />
-        <CalButton label={"C"} onClick={handleButtonClick} />
-        <CalButton label={0} onClick={handleButtonClick} />
->        <c><CalButton label={"="} onClick={handleButtonClick} /></c>
-        <CalButton label={"+"} onClick={handleButtonClick} />
-      </div>
-      <div className="ButtonContainer">
-      <CalButton label={"MANESE"} onClick={() => {}} />
+        <CalcButton label={7} onClick={handleButtonClick} />
+        <CalcButton label={8} onClick={handleButtonClick} />
+        <CalcButton label={9} onClick={handleButtonClick} />
+        <CalcButton label="+" onClick={handleButtonClick} />
+        <CalcButton label={4} onClick={handleButtonClick} />
+        <CalcButton label={5} onClick={handleButtonClick} />
+        <CalcButton label={6} onClick={handleButtonClick} />
+        <CalcButton label="-" onClick={handleButtonClick} />
+        <CalcButton label={1} onClick={handleButtonClick} />
+        <CalcButton label={2} onClick={handleButtonClick} />
+        <CalcButton label={3} onClick={handleButtonClick} />
+        <CalcButton label="*" onClick={handleButtonClick} />
+        <CalcButton label="C" onClick={handleButtonClick} />
+        <CalcButton label={0} onClick={handleButtonClick} />
+        <CalcButton label="=" onClick={handleButtonClick} />
+        <CalcButton label="/" onClick={handleButtonClick} />
+
+         {/* Surname button */}
+         <div className='Surname'>
+         <CalcButton label="AUGUIS" onClick={handleSurnameButtonClick} />
+         </div>
       </div>
     </div>
   );
